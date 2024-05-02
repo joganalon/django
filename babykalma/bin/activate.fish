9e299d5e3af8aee1bd5422fc99f0905bc1145e0f
iamjonel7 <iamjonel7@gmail.com>
2024-05-02 20:43:16 +0800

    update

--- babykalma/bin/activate.fish
+++ babykalma/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jganalon/django/babykalma"
+set -gx VIRTUAL_ENV "/home/jganalon/Github/django/babykalma"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
